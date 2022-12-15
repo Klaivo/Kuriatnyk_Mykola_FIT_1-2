cce0f92904470076fef0e0d34df3a03a30d3a47a
Klaivo <120687440+Klaivo@users.noreply.github.com>
2022-12-16 00:19:47 +0200

    Add files via upload

---
+++ Kuriatnyk_Mykola_FIT_1-2/Kuriatnyk_Mykola_FIT_1-2/Kuriatnyk_Mykola_FIT_1-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <sstream> 
+#include "Windows.h"
+using namespace std;
+void Count()
+{
+    int num;
+    cout << "Ведіть число ";
+    cin >> num;
+    ostringstream os;
+    os << num;
+    cout << endl << "Кількість цифр: " << os.str().size();
+};
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Count();
+    system("pause");
+    return 0;
+}
